798e6992a72df487dfef6af9fa2fc26f77ebb7b0
Sheila Azhar Almufarida <47959466+sheilaazhara@users.noreply.github.com>
2023-09-08 07:22:54 +0700

    feat: implement unleash switch, variant, context

--- plugins/unleash.js
+++ plugins/unleash.js
@@ -4,10 +4,12 @@ export default ({ $config }, inject) => {
   const unleash = new UnleashClient({
     url: $config.unleashURL,
     clientKey: $config.unleashKey,
-    refreshInterval: 30,
+    refreshInterval: 15,
     appName: 'demo-unleash',
   })
 
   inject('unleash', unleash)
   unleash.start()
+  // context
+  unleash.setContextField('userId', 123)
 }
